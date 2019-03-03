10674a2d1220c15ca4a107e2989399f8064b4380
Lance Haig <lnhaig@gmail.com>
2019-03-03 17:47:36 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 rm -f .env
 
-echo DB_SERVER=<DB_ENDPOINT> >> .env
+echo DB_SERVER=$DB_ENDPOINT >> .env
 
 user=$(curl  -H "X-Vault-Token: $VAULT_TOKEN" \
-        -X GET http://<VAULT_ENDPOINT>:8200/v1/database/creds/mynewrole)
+        -X GET http://$VAULT_ENDPOINT:8200/v1/database/creds/userdbrole)
 echo DB_USER=$(echo $user | jq -r .data.username) >> .env
 echo DB_PASSWORD=$(echo $user | jq -r .data.password) >> .env
 
